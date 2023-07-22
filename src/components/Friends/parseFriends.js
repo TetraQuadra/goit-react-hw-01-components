345f3f34aea44f8a6b47431fc29e091b378020b7
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2023-07-22 17:11:38 +0300

    feat: friendlist

--- src/components/Friends/parseFriends.js
+++ src/components/Friends/parseFriends.js
@@ -1,21 +1,31 @@
 const parseFriends = friends => {
-  console.log(friends);
-  //   return friends.map(({ avatar, name, isOnline, id }) => (
-  //     <li key={id} className="item">
-  //       <span
-  //         className="status"
-  //         style={{
-  //           height: 50,
-  //           borderRadius: '50%',
-  //           backgroundColor: isOnline ? 'green' : 'tomato',
-  //         }}
-  //       >
-  //         {isOnline}
-  //       </span>
-  //       <img className="avatar1" src={avatar} alt={name} width="48px" />
-  //       <p className="name">{name}</p>
-  //     </li>
-  //   ));
+  return friends.friends.map(({ avatar, name, isOnline, id }) => (
+    <li
+      key={id}
+      className="item"
+      style={{
+        display: 'flex',
+        gap: 12,
+        alignItems: 'center',
+        padding: 8,
+        border: '1px solid rgb(153, 153, 153)',
+      }}
+    >
+      <span
+        className="status"
+        style={{
+          height: '24px',
+          width: '24px',
+          borderRadius: '50%',
+          backgroundColor: isOnline ? 'green' : 'tomato',
+        }}
+      >
+        {isOnline}
+      </span>
+      <img className="avatar1" src={avatar} alt={name} width="48px" />
+      <p className="name">{name}</p>
+    </li>
+  ));
 };
 
 export default parseFriends;
